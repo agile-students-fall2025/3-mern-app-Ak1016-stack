79f889a2556f0de87f561b242de0165a5ac16598
Ak1016-stack <ak10747@nyu.edu>
2025-10-02 20:15:45 -0400

    Created About.js

---
+++ front-end/src/About.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react'
+import './About.css'
+
+const About = () => {
+    const [data, setData] = useState(null)
+
+    useEffect(() => {
+        fetch('/api/about')
+            .then(res => res.json())
+            .then(setData)
+    }, [])
+
+    if (!data) return null
+
+    return (
+        <>
+          <main className="About">
+            <h1>{data.title}</h1>
+            <img src={data.photoUrl} alt={data.name} />
+            <h2>{data.name}</h2>
+            {data.paragraphs && data.paragraphs.map((p, i) => <p key={i}>{p}</p>)}
+          </main>
+        </>
+    )
+}
+
+export default About
